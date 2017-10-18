c60cd0f0264d59bbf7104cd2edc9a9d39f000026
tangbao <i@tbis.me>
2017-10-18 00:32:45 -0400

    版本2，日了他妈玄学bug

---
+++ app/src/main/java/me/tbis/contactlist/MyInterface.java
@@ -0,0 +1,22 @@
+package me.tbis.contactlist;
+
+/**
+ * Created by tzzma on 2017/10/18.
+ */
+
+public class MyInterface {
+
+    interface OnContactSelectedListener{
+        void onContactSelected(ContactInfo contactInfo);
+    }
+
+
+    interface OnAddReturnListener{
+        void onAddReturn(ContactInfo contactInfo);
+    }
+
+
+    interface OnAddClickPListener{
+        void onAddClickP();
+    }
+}
